7f315580c8f967f16d85dd0f2eca80c3fa138f03
Jalal <jalalk97@hotmail.com>
2022-10-14 04:11:40 -0400

    Exercise 1.7: unicafe step2

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,29 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const incrementGood = () => setGood(good + 1);
-  const incrementNeutral = () => setNeutral(neutral + 1);
-  const incrementBad = () => setBad(bad + 1);
+  const incrementGood = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+  }
+
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+  }
+
+  const incrementBad = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+  }
+
+  const average = () => (good - bad) / total;
+
+  const positiveRate = () => (100 * good) / total;
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick={incrementGood} text='good' />
-      <Button onClick={incrementNeutral} text='neutral' />
-      <Button onClick={incrementBad} text='bad' />
+      <Button onClick={incrementGood} text="good" />
+      <Button onClick={incrementNeutral} text="neutral" />
+      <Button onClick={incrementBad} text="bad" />
       <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {average()}</div>
+      <div>positive {positiveRate()} %</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
