8c8a4afed6ef9eff9be93d6b02caeacd078f891a
Jalal <jalalk97@hotmail.com>
2022-10-19 14:57:24 -0400

    Exercise 3.6: Phonebook backend step6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const PORT = 3001;
 const app = express();
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -46,6 +48,42 @@ app.delete("/api/persons/:id", (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+const generateId = () => {
+  let id;
+  do {
+    id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+  } while (persons.find((person) => person.id === id));
+  return id;
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: "name missing",
+    });
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number missing",
+    });
+  }
+  if (persons.find((person) => person.name == body.name)) {
+    return response.status(400).json({
+      error: `name must be unique`,
+    });
+  }
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+  persons = persons.concat(person);
+
+  response.status(201).json(person);
 });
 
 app.get("/info", (request, response) => {
