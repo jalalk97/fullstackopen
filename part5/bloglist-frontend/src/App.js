4dd32f118908599b3f25d20ba5603fdd8bda5676
Jalal <jalalk97@hotmail.com>
2022-11-02 15:10:09 -0400

    Exercise 5.1: bloglist frontend, step1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,22 +1,54 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState(null)
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log("Submit login")
+
+    try {
+      const user = await loginService.login({ username, password })
+      console.log(user)
+      setUser(user);
+      setUsername("")
+      setPassword("")
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user == null
+        ? <LoginForm
+          handleSubmit={handleLogin}
+          handleUsernameChange={(event) => setUsername(event.target.value)}
+          handlePasswordChange={(event) => setPassword(event.target.value)}
+          username={username}
+          password={password}
+        />
+        : <>
+          <h2>blogs</h2>
+          <p>{user.name} logged in</p>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />
+          )}
+        </>
+      }
     </div>
   )
 }
---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,37 @@
+const LoginForm = ({
+  handleSubmit,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password,
+}) => {
+
+  return (
+    <>
+      <h2>log in to application</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">username</label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">password</label>
+          <input
+            type="password"
+            name="username"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </>
+  )
+}
+
+export default LoginForm
