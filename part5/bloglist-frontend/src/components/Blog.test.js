34aca26b9380ae1de0a1d97040a065c3aa69ab01
Jalal <jalalk97@hotmail.com>
2022-11-07 21:04:30 -0500

    Exercise 5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
-test('renders title and author', () => {
+test('renders title and author but not url and likes', () => {
   const blog = {
     title: 'The blog title',
     author: 'The blog author',
@@ -21,8 +22,6 @@ test('renders title and author', () => {
     showDelete={false}
   />)
 
-  screen.debug()
-
   const blogDiv = container.querySelector('.blog')
 
   expect(blogDiv).toHaveTextContent(
@@ -35,6 +34,39 @@ test('renders title and author', () => {
     'https://www.theblog.com'
   )
   expect(blogDiv).not.toHaveTextContent(
+    'likes'
+  )
+})
+
+test('renders url and likes on button click', async () => {
+  const blog = {
+    title: 'The blog title',
+    author: 'The blog author',
+    url: 'https://www.theblog.com',
+    user: {
+      name: 'User Name',
+      username: 'username',
+    },
+  }
+
+  const mockHandler = jest.fn()
+
+  const { container } = render(<Blog
+    blog={blog}
+    likeBlog={() => console.log('like')}
+    removeBlog={() => console.log('remove')}
+    showDelete={false}
+  />)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  const blogDiv = container.querySelector('.blog')
+  await user.click(button)
+
+  expect(blogDiv).toHaveTextContent(
+    'https://www.theblog.com'
+  )
+  expect(blogDiv).toHaveTextContent(
     'likes'
   )
 })
