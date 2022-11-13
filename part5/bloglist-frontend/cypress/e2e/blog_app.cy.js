68edc7a7a2b447ab50fa7a32eece0f731b432a82
Jalal <jalalk97@hotmail.com>
2022-11-13 16:57:36 -0500

    Exercise 5.18: bloglist end to end testing, step2

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,13 @@
 describe("Blog app", function () {
   beforeEach(function () {
+    const user = {
+      name: "Big Bob",
+      username: "bigbob1256",
+      password: "6521bobgib",
+    };
+
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
 
@@ -10,5 +17,25 @@ describe("Blog app", function () {
     cy.get("form").find("label").contains("username");
     cy.get("form").find("label").contains("password");
     cy.get("form").find("button").contains("login");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("input[name='username']").type("bigbob1256");
+      cy.get("input[name='password']").type("6521bobgib");
+      cy.contains("login").click();
+      cy.contains("Big Bob logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("input[name='username']").type("bigbob1256");
+      cy.get("input[name='password']").type("bigbob1256");
+      cy.contains("login").click();
+      cy.contains("invalid username or password").should(
+        "have.css",
+        "color",
+        "rgb(255, 0, 0)"
+      );
+    });
   });
 });
