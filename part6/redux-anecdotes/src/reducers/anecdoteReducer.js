8c92c2f702f67bad6acea75e2e5a3f6f1c9a964c
Jalal <jalalk97@hotmail.com>
2022-11-16 23:29:05 -0500

    Exercise 6.5: anecdotes, step3

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -29,9 +29,11 @@ const reducer = (state = initialState, action) => {
         ...anecdoteToChange,
         votes: anecdoteToChange.votes + 1,
       };
-      return state.map((anecdote) =>
-        anecdote.id === action.payload ? changedAnecdote : anecdote
-      );
+      return state
+        .map((anecdote) =>
+          anecdote.id === action.payload ? changedAnecdote : anecdote
+        )
+        .sort((a1, a2) => a2.votes - a1.votes);
     case "NEW_ANECDOTES":
       return [...state, action.payload];
   }
