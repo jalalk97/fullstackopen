1c85cf12144d368d6907f8f0f2c3871c61474923
Jalal <jalalk97@hotmail.com>
2023-02-08 00:34:19 -0500

    Exercise 8.3: All authors

--- part8/library/index.js
+++ part8/library/index.js
@@ -84,6 +84,7 @@ const typeDefs = `
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 
   type Book {
@@ -93,6 +94,13 @@ const typeDefs = `
     genres: [String!]!
     id: ID!
   }
+
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
 `;
 
 const resolvers = {
@@ -100,6 +108,11 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () => authors,
+  },
+  Author: {
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length,
   },
 };
 
